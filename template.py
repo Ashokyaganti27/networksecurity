6443b1d9e3e33d9f1b9ec504e39cfaa470c82148
yagantiAshok <yagantiashok177@gmail.com>
2025-01-16 10:21:57 +0530

    successfully created folders

---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+
+
+
+list_of_files=[
+    ".github/workflows/main.yaml",
+    "networksecurity/__init__.py",
+    "networksecurity/constants/__init__.py",
+    "networksecurity/utils/__init__.py",
+    "networksecurity/entity/__init__.py",
+    "networksecurity/components/__init__.py",
+    "networksecurity/pipeline/__init__.py",
+    "networksecurity/config/__init__.py",
+    "networksecurity/logging/__init__.py",
+    "networksecurity/exceptions/__init__.py",
+    "setup.py",
+    "DockerFile",
+]
+
+
+for files in list_of_files:
+    filepath=Path(files)
+    filedir,filename=os.path.split(filepath)
+
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"filediectory successfully created at {filepath}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)):
+
+        with open(filepath,"w") as file:
+            pass
+        logging.info(f"filename successfully created at {filepath}")
+    else:
+        logging.info(f"filename already exists {filename}")
+
+
+
+
+
+
